18ba043773673d85140849f2550e7c9579a2c0dc
Dylan <cristhian.butron@ucb.edu.bo>
2024-09-13 14:57:27 -0400

    Vistas docentes
    Cambio de la vista de docentes - estudiantes (datos genericos)
    Cambio de la vista de docentes - materias (datos genericos)
    Cambio de la vista de docentes - horarios (datos genericos)

--- Frontend/src/main.js
+++ Frontend/src/main.js
@@ -7,6 +7,8 @@ import MateriasEstudiante from './views/MateriasEstudiante.vue';
 import InicioDocente from './views/InicioDocente.vue';
 import MateriasDocente from './views/MateriasDocente.vue';
 import PaginaPrincipal from './views/PaginaPrincipal.vue';
+import EstudiantesDocente from './views/EstudiantesDocente.vue';
+import HorariosDocente from './views/HorariosDocente.vue';
 
 // Definir rutas
 const routes = [
@@ -14,7 +16,9 @@ const routes = [
   { path: '/inicio-estudiante', component: InicioEstudiante },
   { path: '/materias-estudiante', component: MateriasEstudiante },
   { path: '/inicio-docente', component: InicioDocente },
-  { path: '/materias-docente', component: MateriasDocente }
+  { path: '/materias-docente', component: MateriasDocente },
+  { path: '/horarios-docente', component: HorariosDocente },
+  { path: '/estudiantes-docente', component: EstudiantesDocente }
 ];
 
 // Configuración del router
