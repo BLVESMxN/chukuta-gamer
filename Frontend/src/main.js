ae0badc2b6e8f8fd4685c5db4a0bf9e423e94374
Pollo69 <rodrigo.tarifa@ucb.edu.bo>
2024-09-23 18:05:24 -0400

    arreglando la wea

--- Frontend/src/main.js
+++ Frontend/src/main.js
@@ -2,19 +2,31 @@ import { createApp } from "vue";
 import App from "./App.vue";
 import { createRouter, createWebHistory } from "vue-router";
 
-import InicioEstudiante from './views/InicioEstudiante.vue';
-import MateriasEstudiante from './views/MateriasEstudiante.vue';
-import InicioDocente from './views/InicioDocente.vue';
-import MateriasDocente from './views/MateriasDocente.vue';
-import PaginaPrincipal from './views/PaginaPrincipal.vue';
+import InicioEstudiante from "./views/InicioEstudiante.vue";
+import MateriasEstudiante from "./views/MateriasEstudiante.vue";
+import InicioDocente from "./views/InicioDocente.vue";
+import MateriasDocente from "./views/MateriasDocente.vue";
+import PaginaPrincipal from "./views/PaginaPrincipal.vue";
+import EstudiantesDocente from "./views/EstudiantesDocente.vue";
+import HorariosDocente from "./views/HorariosDocente.vue";
+import GradosAdmin from "./views/GradosAdmin.vue";
+import AsignaturasAdmin from "./views/AsignaturasAdmin.vue";
+import prueba from "./views/previews/PruebaCrud.vue";
+
+import { RequestHandler } from "./controlador/RequestHandler.mjs";
 
 // Definir rutas
 const routes = [
-  { path: '/', component: PaginaPrincipal },
-  { path: '/inicio-estudiante', component: InicioEstudiante },
-  { path: '/materias-estudiante', component: MateriasEstudiante },
-  { path: '/inicio-docente', component: InicioDocente },
-  { path: '/materias-docente', component: MateriasDocente }
+  { path: "/", component: PaginaPrincipal },
+  { path: "/inicio-estudiante", component: InicioEstudiante },
+  { path: "/materias-estudiante", component: MateriasEstudiante },
+  { path: "/inicio-docente", component: InicioDocente },
+  { path: "/materias-docente", component: MateriasDocente },
+  { path: "/horarios-docente", component: HorariosDocente },
+  { path: "/estudiantes-docente", component: EstudiantesDocente },
+  { path: "/grados-admin", component: GradosAdmin },
+  { path: "/asignaturas-admin", component: AsignaturasAdmin },
+  { path: "/prueba-crud", component: prueba },
 ];
 
 // Configuración del router
