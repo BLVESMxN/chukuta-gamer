fa5b63f7349820c6aaab17dcbc5defac0c9b8d83
OSM <andrewing.fis@gmail.com>
2024-10-06 22:56:38 -0300

    LOGIN

--- Frontend/src/components/NavbarComponent.vue
+++ Frontend/src/components/NavbarComponent.vue
@@ -2,40 +2,89 @@
   <nav class="navbar">
     <div class="navbar-left">
       <!-- Guest Navbar -->
-      <span v-if="userRole === 'guest'">
+      <span v-if="userRole.nombre === 'Guest'">
         <button @click="toggleMenu" class="menu-button">☰</button>
         <span class="title">SISTEMA DE ESTUDIANTES</span>
       </span>
 
       <!-- Estudiante Navbar -->
-      <span v-if="userRole === 'estudiante'">
-        <router-link to="/inicio-estudiante" :class="{ active: isActive('/inicio-estudiante') }" class="nav-link">INICIO</router-link>
-        <router-link to="/materias-estudiante" :class="{ active: isActive('/materias-estudiante') }" class="nav-link">MATERIAS</router-link>
+      <span v-if="userRole.nombre === 'Estudiante'">
+        <router-link
+          to="/inicio-estudiante"
+          :class="{ active: isActive('/inicio-estudiante') }"
+          class="nav-link"
+          >INICIO</router-link
+        >
+        <router-link
+          to="/materias-estudiante"
+          :class="{ active: isActive('/materias-estudiante') }"
+          class="nav-link"
+          >MATERIAS</router-link
+        >
       </span>
 
       <!-- Profesor Navbar -->
-      <span v-if="userRole === 'profesor'">
-        <router-link to="/inicio-docente" :class="{ active: isActive('/inicio-docente') }" class="nav-link">INICIO</router-link>
-        <router-link to="/materias-docente" :class="{ active: isActive('/materias-docente') }" class="nav-link">MATERIAS</router-link>
-        <router-link to="/estudiantes-docente" :class="{ active: isActive('/estudiantes-docente') }" class="nav-link">ESTUDIANTES</router-link>
-        <router-link to="/horarios-docente" :class="{ active: isActive('/horarios-docente') }" class="nav-link">HORARIOS</router-link>
+      <span v-if="userRole.nombre === 'Profesor'">
+        <router-link
+          to="/inicio-docente"
+          :class="{ active: isActive('/inicio-docente') }"
+          class="nav-link"
+          >INICIO</router-link
+        >
+        <router-link
+          to="/materias-docente"
+          :class="{ active: isActive('/materias-docente') }"
+          class="nav-link"
+          >MATERIAS</router-link
+        >
+        <router-link
+          to="/estudiantes-docente"
+          :class="{ active: isActive('/estudiantes-docente') }"
+          class="nav-link"
+          >ESTUDIANTES</router-link
+        >
+        <router-link
+          to="/horarios-docente"
+          :class="{ active: isActive('/horarios-docente') }"
+          class="nav-link"
+          >HORARIOS</router-link
+        >
       </span>
     </div>
 
     <div class="navbar-right">
       <!-- Login Button for Guests -->
-      <button v-if="userRole === 'guest'" @click="showLogin = true" class="login-button">
-        <img src="@/assets/usuario_icon.png" alt="user-icon" class="user-icon" />
+      <button
+        v-if="userRole.nombre === 'Guest'"
+        @click="showLogin = true"
+        class="login-button"
+      >
+        <img
+          src="@/assets/usuario_icon.png"
+          alt="user-icon"
+          class="user-icon"
+        />
       </button>
 
       <!-- User Panel for Logged-In Users -->
       <div v-else class="user-panel">
-        <img src="@/assets/usuario_icon.png" alt="user-icon" class="user-icon" @click="toggleUserOptions" />
+        <img
+          src="@/assets/usuario_icon.png"
+          alt="user-icon"
+          class="user-icon"
+          @click="toggleUserOptions"
+        />
         <div v-if="showUserOptions" class="user-options-panel">
           <p class="user-name">{{ username }}</p>
-          <router-link to="/editar-datos" class="user-option">Editar datos personales</router-link>
-          <router-link to="/cambiar-contrasena" class="user-option">Cambio de contraseña</router-link>
-          <button @click="logout" class="user-option logout-button">Cerrar Sesión</button>
+          <router-link to="/editar-datos" class="user-option"
+            >Editar datos personales</router-link
+          >
+          <router-link to="/cambiar-contrasena" class="user-option"
+            >Cambio de contraseña</router-link
+          >
+          <button @click="logout" class="user-option logout-button">
+            Cerrar Sesión
+          </button>
         </div>
       </div>
     </div>
@@ -46,8 +95,18 @@
         <span class="close" @click="showLogin = false">&times;</span>
         <h2 class="modal-title">Iniciar Sesión</h2>
         <div class="modal-body">
-          <input type="text" placeholder="Usuario" v-model="usernameInput" class="input-field" />
-          <input type="password" placeholder="Contraseña" v-model="passwordInput" class="input-field" />
+          <input
+            type="text"
+            placeholder="Usuario"
+            v-model="usernameInput"
+            class="input-field"
+          />
+          <input
+            type="password"
+            placeholder="Contraseña"
+            v-model="passwordInput"
+            class="input-field"
+          />
           <button @click="login" class="login-button">Ingresar</button>
         </div>
       </div>
@@ -56,6 +115,8 @@
 </template>
 
 <script>
+import { Role } from '../controlador/Session';
+
 export default {
   data() {
     return {
@@ -67,14 +128,13 @@ export default {
   },
   computed: {
     userRole() {
-      const user = this.$session.user;
-      if (!user || this.$session.isAnonymous()) {
-        return 'guest';
+      if (this.$session.isAnonymous()) {
+        return Role.GUEST;
       }
-      return user.role.nombre.toLowerCase();
+      return this.$session.state.user.role;
     },
     username() {
-      return this.$session.user.name || '';
+      return this.$session.state.user.name || '';
     },
   },
   methods: {
@@ -90,13 +150,15 @@ export default {
         this.showLogin = false;
         this.usernameInput = '';
         this.passwordInput = '';
-        // Redirect based on role
-        if (this.userRole === 'estudiante') {
+
+        console.log(this.$Session.getInstance().state.user.role);
+        console.log(this.userRole);
+
+        if (this.userRole.nombre === 'Estudiante') {
           this.$router.push('/inicio-estudiante');
-        } else if (this.userRole === 'profesor') {
+        } else if (this.userRole.nombre === 'Profesor') {
           this.$router.push('/inicio-docente');
         } else {
-          // Default redirect if role doesn't match
           this.$router.push('/');
         }
       } catch (error) {
@@ -107,7 +169,7 @@ export default {
       try {
         await this.$Session.logout();
         this.showUserOptions = false;
-        this.$router.push('/'); // Redirect to home page after logout
+        this.$router.push('/'); 
       } catch (error) {
         alert('Error al cerrar sesión: ' + error.message);
       }
