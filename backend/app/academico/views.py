eec4ffe6101fe2edb13bc54ee929fe0d5ff1d0d7
OSM <andrewing.fis@gmail.com>
2024-10-07 12:19:19 -0300

    Error queryset

--- backend/app/academico/views.py
+++ backend/app/academico/views.py
@@ -177,7 +177,11 @@ class EstudianteViewSet(viewsets.ModelViewSet):
         elif user.role == Role.get_student():
             return estudiantes.filter(id=user.estudiante.id)
         elif user.role == Role.get_parent():
-            return estudiantes.get_children()
+            estudiantes = user.padre_estudiante.get_children()
+            estudiantes = estudiantes | user.madre_estudiante.get_children ()
+            if qp_colegio:
+                return estudiantes.filter(colegio=qp_colegio)
+            return estudiantes
         return Estudiante.objects.none()
 
     def perform_create(self, serializer):
